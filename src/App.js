9047b80f7e1bdfe8fb6deb85613aa391dc529c5d
kuntida0709 <oing1602@gmail.com>
2026-01-15 10:17:27 +0700

    add refs-array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { EventData2 } from "./even-data";
+import RefsArray from "./refs-array";
 
 function App() {
   return (
-    <>
-      <EventData2/>
-    </>
+    
+      <RefsArray/>
+    
   )
 }
 
