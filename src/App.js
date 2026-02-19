68112b4448c8388ca7d62002441669440e547535
kuntida0709 <oing1602@gmail.com>
2026-02-19 11:31:08 +0700

    formtext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import Router3 from "./router3";
+import FormText from "./form-text";
 export default function App() {
   return (
-    <Router3/>
+    <FormText/>
     
   )
 }
