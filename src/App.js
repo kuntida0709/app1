a88d0fd5eea54dd573529a00bf6b573bfeec3e86
kuntida0709 <oing1602@gmail.com>
2026-02-26 10:06:29 +0700

    form select file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-import FormText from "./form-text";
+import FormSelectFile from "./form-select-file";
+
 export default function App() {
   return (
-    <FormText/>
+    <FormSelectFile/>
     
   )
 }
